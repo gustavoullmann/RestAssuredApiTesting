9c74a7b1c1a36940e15f578f97ea1c21e0a643ec
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-20 10:37:20 -0300

    criado método 'ValidaApiOnline'. Função '.time' e respectivas bibliotecas deixadas comentadas

--- src/test/java/ClasseDeTeste.java
+++ src/test/java/ClasseDeTeste.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ClasseDeTeste {
+import io.restassured.response.Response;
+import org.junit.Test;
+
+//import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.given;
+//import static org.hamcrest.Matchers.lessThan;
+
+public class ClasseDeTeste {
+
+    @Test
+    public void validaApiOnline() {
+        Response responsePing =
+                given()
+                        .header("Content-Type", "application/json")
+                        .when()
+                        .get("https://treinamento-api.herokuapp.com/ping");
+
+        responsePing
+                .then()
+                .statusCode(201);
+//                .time(lessThan(2L), TimeUnit.SECONDS);
+    }
 }
