a64163bc9c1b77030d4c30ae35dd1aabba615df1
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-21 11:22:00 -0300

    criado método: 'jsonAuthLogin()'

--- src/test/java/br/com/restassuredapitesting/tests/auth/requests/payload/AuthPayloads.java
+++ src/test/java/br/com/restassuredapitesting/tests/auth/requests/payload/AuthPayloads.java
@@ -1,2 +1,15 @@
-package br.com.restassuredapitesting.tests.auth.requests.payload;public class AuthPayloads {
+package br.com.restassuredapitesting.tests.auth.requests.payload;
+
+import org.json.JSONObject;
+
+public class AuthPayloads {
+
+    public JSONObject jsonAuthLogin() {
+        JSONObject payloadLogin = new JSONObject();
+
+        payloadLogin.put("username", "admin");
+        payloadLogin.put("password", "password123");
+
+        return payloadLogin;
+    }
 }
