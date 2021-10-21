5ca6c29e90c33c6a8bb7a56ac9e179deeec52414
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-21 18:18:28 -0300

    criado metodo validaSchemaDaListagemDeReservas()

--- src/test/java/br/com/restassuredapitesting/tests/booking/tests/GetBookingTest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/tests/GetBookingTest.java
@@ -2,10 +2,15 @@ package br.com.restassuredapitesting.tests.booking.tests;
 
 import br.com.restassuredapitesting.base.BaseTest;
 import br.com.restassuredapitesting.suites.AllTests;
+import br.com.restassuredapitesting.suites.ContractTests;
 import br.com.restassuredapitesting.tests.booking.requests.GetBookingRequest;
+import br.com.restassuredapitesting.utils.Utils;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.io.File;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static org.hamcrest.Matchers.greaterThan;
 
 public class GetBookingTest extends BaseTest {
@@ -20,5 +25,15 @@ public class GetBookingTest extends BaseTest {
                 .then()
                 .statusCode(200)
                 .body("size()", greaterThan(0));
+    }
+
+    @Test
+    @Category({AllTests.class, ContractTests.class})
+    public void validaSchemaDaListagemDeReservas() {
+
+        getBookingRequest.bookingReturnIds()
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchema(new File(Utils.getSchemaBasePath("booking","bookings"))));
     }
 }
