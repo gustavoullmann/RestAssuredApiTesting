3da11f5384079f423bf1de434a57fc93e67a7a2d
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-21 11:05:30 -0300

    criados métodos: 'tokenReturs()' e 'getToken()'

--- src/test/java/br/com/restassuredapitesting/tests/auth/requests/PostAuthRequest.java
+++ src/test/java/br/com/restassuredapitesting/tests/auth/requests/PostAuthRequest.java
@@ -1,2 +1,29 @@
-package br.com.restassuredapitesting.tests.auth.requests;public class PostAuthRequest {
+package br.com.restassuredapitesting.tests.auth.requests;
+
+import io.restassured.response.Response;
+import org.json.JSONObject;
+
+import static io.restassured.RestAssured.given;
+
+public class PostAuthRequest {
+
+    public Response tokenReturs() {
+        JSONObject payload = new JSONObject();
+        payload.put("username","admin");
+        payload.put("password", "password123");
+
+        return given()
+                .header("Content-Type", "application/json")
+                .when()
+                .body(payload.toString())
+                .post("https://treinamento-api.herokuapp.com/auth");
+    }
+
+    public String getToken() {
+        return "token=" + this.tokenReturs()
+                .then()
+                .statusCode(200)
+                .extract()
+                .path("token");
+    }
 }
