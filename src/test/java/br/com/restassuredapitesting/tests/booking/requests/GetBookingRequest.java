1849b2709ef7ff90b105e570ac26d59ed2bff913
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-20 21:26:34 -0300

    criado método response para o request bookingReturnIds()

--- src/test/java/br/com/restassuredapitesting/tests/booking/requests/GetBookingRequest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/requests/GetBookingRequest.java
@@ -1,2 +1,12 @@
-package br.com.restassuredapitesting.tests.booking.requests;public class GetBookingRequest {
+package br.com.restassuredapitesting.tests.booking.requests;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class GetBookingRequest {
+
+    public Response bookingReturnIds() {
+        return given().when().get("https://treinamento-api.herokuapp.com/booking");
+    }
 }
