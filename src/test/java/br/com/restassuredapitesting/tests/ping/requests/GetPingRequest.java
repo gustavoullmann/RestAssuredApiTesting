aeba06538e4da883bd78e3e7b45b83b6490abe54
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-20 22:11:47 -0300

    renomeado 'ClasseDeTeste' p/ 'GetPingTest'. Criada classe 'GetPingRequest'

---
+++ src/test/java/br/com/restassuredapitesting/tests/ping/requests/GetPingRequest.java
@@ -0,0 +1,2 @@
+package br.com.restassuredapitesting.tests.ping.requests;public class GetPingRequest {
+}
---
+++ src/test/java/br/com/restassuredapitesting/tests/ping/tests/GetPingTest.java
@@ -0,0 +1,24 @@
+package br.com.restassuredapitesting.tests;
+
+import br.com.restassuredapitesting.suites.AllTests;
+import br.com.restassuredapitesting.tests.ping.requests.GetPingRequest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+//import java.util.concurrent.TimeUnit;
+//import static org.hamcrest.Matchers.lessThan;
+
+public class GetPingTest {
+
+    GetPingRequest getPingRequest = new GetPingTest();
+
+    @Test
+    @Category({AllTests.class})
+    public void validaApiOnline() {
+
+        getPingRequest.pingReturnApi()
+                .then()
+                .statusCode(201);
+//              .time(lessThan(2L), TimeUnit.SECONDS);
+    }
+}
