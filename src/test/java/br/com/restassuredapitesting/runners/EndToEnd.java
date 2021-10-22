5c96d371a682af05cbbb4dcdf5c7a38fe548d388
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-22 20:40:34 -0300

    incluidos @runwith, @categories e @suite

--- src/test/java/br/com/restassuredapitesting/runners/EndToEnd.java
+++ src/test/java/br/com/restassuredapitesting/runners/EndToEnd.java
@@ -1,2 +1,14 @@
-package br.com.restassuredapitesting.runners;public class EndToEnd {
+package br.com.restassuredapitesting.runners;
+
+import br.com.restassuredapitesting.tests.booking.tests.DeleteBookingTest;
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory(br.com.restassuredapitesting.suites.EndToEnd.class)
+@Suite.SuiteClasses({
+        DeleteBookingTest.class
+})
+public class EndToEnd {
 }
