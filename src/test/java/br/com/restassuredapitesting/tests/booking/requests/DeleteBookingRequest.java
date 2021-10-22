8f9e405d26384c35e353697fcfbba5a6e6005557
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-22 19:32:39 -0300

    criado método 'deleteBookingToken()'

--- src/test/java/br/com/restassuredapitesting/tests/booking/requests/DeleteBookingRequest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/requests/DeleteBookingRequest.java
@@ -1,2 +1,18 @@
-package br.com.restassuredapitesting.tests.booking.requests;public class DeleteBookingRequest {
+package br.com.restassuredapitesting.tests.booking.requests;
+
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class DeleteBookingRequest {
+
+    @Step("Exclui uma Reserva específica com o parâmetro token")
+    public Response deleteBookingToken(int id, String token) {
+        return given()
+                .header("Content-Type", "application/json")
+                .header("Cookie", token)
+                .when()
+                .delete("booking/" + id);
+    }
 }
