c2ce8f371f1f3f45a0d91100c3bfa4b7382ebac5
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-22 19:33:44 -0300

    criado o teste 'deletaUmaReserva()'

--- src/test/java/br/com/restassuredapitesting/tests/booking/tests/DeleteBookingTest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/tests/DeleteBookingTest.java
@@ -1,2 +1,39 @@
-package br.com.restassuredapitesting.tests.booking.tests;public class DeleteBookingTest {
+package br.com.restassuredapitesting.tests.booking.tests;
+
+import br.com.restassuredapitesting.base.BaseTest;
+import br.com.restassuredapitesting.suites.AcceptanceTests;
+import br.com.restassuredapitesting.suites.AllTests;
+import br.com.restassuredapitesting.tests.auth.requests.PostAuthRequest;
+import br.com.restassuredapitesting.tests.booking.requests.DeleteBookingRequest;
+import br.com.restassuredapitesting.tests.booking.requests.GetBookingRequest;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Feature("Feature - Exclusão de Reservas")
+public class DeleteBookingTest extends BaseTest {
+
+    DeleteBookingRequest deleteBookingRequest = new DeleteBookingRequest();
+    GetBookingRequest getBookingRequest = new GetBookingRequest();
+    PostAuthRequest postAuthRequest = new PostAuthRequest();
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Category({AllTests.class, AcceptanceTests.class})
+    @DisplayName("Excluir uma reserva válida utilizando um token válido")
+    public void deletaUmaReserva() {
+        int primeiroId = getBookingRequest.bookingReturnIds()
+                .then()
+                .statusCode(200)
+                .extract()
+                .path("[0].bookingid");
+
+        deleteBookingRequest.deleteBookingToken(primeiroId, postAuthRequest.getToken())
+                .then()
+                .statusCode(201);
+//              .body()???
+    }
 }
