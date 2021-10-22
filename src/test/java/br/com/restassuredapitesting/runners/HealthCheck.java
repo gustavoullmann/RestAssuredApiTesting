da9270ac531f542f15e40e693f4749a84f2c79e6
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-22 15:05:38 -0300

    inclusão de @RunWith, @Categories e @Suite

--- src/test/java/br/com/restassuredapitesting/runners/HealthCheck.java
+++ src/test/java/br/com/restassuredapitesting/runners/HealthCheck.java
@@ -1,2 +1,15 @@
-package br.com.restassuredapitesting.runners;public class HealthCheck {
+package br.com.restassuredapitesting.runners;
+
+import br.com.restassuredapitesting.tests.healthcheck.tests.HealthCheckTest;
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory(br.com.restassuredapitesting.suites.HealthCheck.class)
+@Suite.SuiteClasses({
+        HealthCheckTest.class
+})
+
+public class HealthCheck {
 }
