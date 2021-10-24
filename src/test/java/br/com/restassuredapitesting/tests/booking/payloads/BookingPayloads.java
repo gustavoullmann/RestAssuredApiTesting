47954ca4aa47ca46505284ef9904e165fe9f29d8
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-23 23:04:14 -0300

    criado json 'payloadCreateValidBooking'

--- src/test/java/br/com/restassuredapitesting/tests/booking/payloads/BookingPayloads.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/payloads/BookingPayloads.java
@@ -20,4 +20,21 @@ public class BookingPayloads {
 
         return payload;
     }
+
+    public static JSONObject payloadCreateValidBooking() {
+        JSONObject payload = new JSONObject();
+        JSONObject bookingDates = new JSONObject();
+
+        bookingDates.put("checkin", "2020-01-01");
+        bookingDates.put("checkout", "2021-01-01");
+
+        payload.put("firstname", "Teste");
+        payload.put("lastname", "Testando");
+        payload.put("totalprice", 222);
+        payload.put("depositpaid", true);
+        payload.put("bookingdates", bookingDates);
+        payload.put("additionalneeds", "realizar todos os testes");
+
+        return payload;
+    }
 }
