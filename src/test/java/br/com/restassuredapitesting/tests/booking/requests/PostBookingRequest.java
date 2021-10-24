f67ce5ff8ede1d2aea38706bb6c54bbc0d2545b9
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-24 19:39:19 -0300

    método createBooking() movido de put para post

--- src/test/java/br/com/restassuredapitesting/tests/booking/requests/PostBookingRequest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/requests/PostBookingRequest.java
@@ -1,2 +1,21 @@
-package br.com.restassuredapitesting.tests.booking.requests;public class PostBookingRequest {
+package br.com.restassuredapitesting.tests.booking.requests;
+
+import br.com.restassuredapitesting.tests.booking.payloads.BookingPayloads;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class PostBookingRequest {
+
+    BookingPayloads bookingPayloads = new BookingPayloads();
+
+    @Step("Cria uma reserva")
+    public Response createBooking() {
+        return given()
+                .header("Content-Type", "application/json")
+                .when()
+                .body(bookingPayloads.payloadCreateValidBooking().toString())
+                .post("booking/");
+    }
 }
