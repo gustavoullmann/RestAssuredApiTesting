373573a9f2193359a40c51efeecbd645d71f771e
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-24 00:31:27 -0300

    criado metodo 'updateBookingAuthorization(int id)'

--- src/test/java/br/com/restassuredapitesting/tests/booking/requests/PutBookingRequest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/requests/PutBookingRequest.java
@@ -17,7 +17,7 @@ public class PutBookingRequest {
                 .header("Accept", "application/json")
                 .header("Cookie", token)
                 .when()
-                .body(bookingPayloads.payloadValidBooking().toString())
+                .body(bookingPayloads.payloadValidBookingToken().toString())
                 .put("booking/" + id);
     }
 
@@ -28,5 +28,16 @@ public class PutBookingRequest {
                 .when()
                 .body(bookingPayloads.payloadCreateValidBooking().toString())
                 .post("booking/");
+    }
+
+    @Step("Atualiza uma Reserva específica com o parâmetro Authorization")
+    public Response updateBookingAuthorization(int id) {
+        return given()
+                .header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=")
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .when()
+                .body(bookingPayloads.payloadValidBookingAuthorization().toString())
+                .put("booking/" + id);
     }
 }
