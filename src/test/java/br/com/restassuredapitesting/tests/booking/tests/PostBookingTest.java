ff3891b318ae702c1f976329a5ab784ff78a980f
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-24 19:42:05 -0300

    teste criarUmaReserva() movido de put para post

--- src/test/java/br/com/restassuredapitesting/tests/booking/tests/PostBookingTest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/tests/PostBookingTest.java
@@ -1,2 +1,32 @@
-package br.com.restassuredapitesting.tests.booking.tests;public class PostBookingTest {
+package br.com.restassuredapitesting.tests.booking.tests;
+
+import br.com.restassuredapitesting.base.BaseTest;
+import br.com.restassuredapitesting.suites.AcceptanceTests;
+import br.com.restassuredapitesting.suites.AllTests;
+import br.com.restassuredapitesting.tests.booking.payloads.BookingPayloads;
+import br.com.restassuredapitesting.tests.booking.requests.PostBookingRequest;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.hamcrest.Matchers.containsString;
+
+public class PostBookingTest extends BaseTest {
+
+    PostBookingRequest postBookingRequest = new PostBookingRequest();
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Category({AllTests.class, AcceptanceTests.class})
+    @DisplayName("Criar uma reserva")
+    public void criarUmaReserva() {
+        String nome = BookingPayloads.payloadCreateValidBooking().getString("firstname");
+
+        postBookingRequest.createBooking()
+                .then()
+                .statusCode(200)
+                .body("booking.firstname", containsString(nome));
+    }
 }
