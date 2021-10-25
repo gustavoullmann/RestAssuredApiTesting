28fcc4faba9dc9aa8f4e4a0d701845ac07296df9
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-24 21:17:12 -0300

    criados testes: criarUmaReservaComPayLoadInvalido(), criarDuasReservasSequenciais(), criarUmaReservaComPayLoadInvalidoMaior()

--- src/test/java/br/com/restassuredapitesting/tests/booking/tests/PostBookingTest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/tests/PostBookingTest.java
@@ -3,6 +3,7 @@ package br.com.restassuredapitesting.tests.booking.tests;
 import br.com.restassuredapitesting.base.BaseTest;
 import br.com.restassuredapitesting.suites.AcceptanceTests;
 import br.com.restassuredapitesting.suites.AllTests;
+import br.com.restassuredapitesting.suites.EndToEnd;
 import br.com.restassuredapitesting.tests.booking.payloads.BookingPayloads;
 import br.com.restassuredapitesting.tests.booking.requests.PostBookingRequest;
 import io.qameta.allure.Severity;
@@ -22,10 +23,55 @@ public class PostBookingTest extends BaseTest {
     @Category({AllTests.class, AcceptanceTests.class})
     @DisplayName("Criar uma reserva")
     public void criarUmaReserva() {
-        String nome = BookingPayloads.payloadCreateValidBooking().getString("firstname");
+        String nome = BookingPayloads.payloadCreateValidBooking1().getString("firstname");
 
-        postBookingRequest.createBooking()
+        postBookingRequest.createBooking(BookingPayloads.payloadCreateValidBooking1())
                 .then()
+                .statusCode(200)
+                .body("booking.firstname", containsString(nome));
+    }
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Category({AllTests.class, EndToEnd.class})
+    @DisplayName("Criar uma reserva com payload inválido")
+    public void criarUmaReservaComPayLoadInvalido() {
+
+        postBookingRequest.createInvalidBooking()
+                .then()
+                .statusCode(500);
+    }
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Category({AllTests.class, AcceptanceTests.class})
+    @DisplayName("Criar duas reservas em sequencia")
+    public void criarDuasReservasSequenciais() {
+        String nome1 = BookingPayloads.payloadCreateValidBooking1().getString("firstname");
+        String nome2 = BookingPayloads.payloadCreateValidBooking2().getString("firstname");
+
+        postBookingRequest.createBooking(BookingPayloads.payloadCreateValidBooking1())
+                .then()
+                .statusCode(200)
+                .body("booking.firstname", containsString(nome1));
+
+        postBookingRequest.createBooking(BookingPayloads.payloadCreateValidBooking2())
+                .then()
+                .statusCode(200)
+                .body("booking.firstname", containsString(nome2));
+    }
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Category({AllTests.class, EndToEnd.class})
+    @DisplayName("Criar uma reserva com payload maior")
+    public void criarUmaReservaComPayLoadInvalidoMaior() {
+
+        String nome = BookingPayloads.payloadCreateInvalidBookingMoreParams().getString("firstname");
+
+        postBookingRequest.createBooking(BookingPayloads.payloadCreateInvalidBookingMoreParams())
+                .then()
+                .log().all()
                 .statusCode(200)
                 .body("booking.firstname", containsString(nome));
     }
