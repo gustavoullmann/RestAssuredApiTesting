e69a78e0951320152f71ac0558cf1037d820b5e4
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-25 19:35:25 -0300

    melhoria na descrição dos @Steps e nomes de métodos

--- src/test/java/br/com/restassuredapitesting/tests/booking/requests/GetBookingRequest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/requests/GetBookingRequest.java
@@ -7,46 +7,44 @@ import static io.restassured.RestAssured.given;
 
 public class GetBookingRequest {
 
-    public int returnFirtsId() {
-        return bookingReturnIds()
+    public int returnBookingIdsListFirstId() {
+        return returnBookingIdsList()
                 .then()
                 .statusCode(200)
                 .extract()
                 .path("[0].bookingid");
     }
 
-    @Step("Retorna os Ids da listagem de Reservas")
-    public Response bookingReturnIds() {
+    @Step("Retorna lista com Id's das Reservas cadastradas")
+    public Response returnBookingIdsList() {
         return given()
                 .when()
                 .get("booking");
     }
 
-    @Step("Retorna a primeira reserva cadastrada")
+    @Step("Retorna Reserva do primeiro Id cadastrado") //TODO: remover esse método e usar o returnSpecificBookingWithId em seu lugar
     public Response bookingReturnFirstId() {
-        int primeiroId = returnFirtsId();
+        int firstId = returnBookingIdsListFirstId();
 
         return given()
                 .when()
-                .get("booking/" + primeiroId);
+                .get("booking/" + firstId);
     }
 
-    @Step("Retorna uma reserva específica cadastrada")
-    public Response bookingReturn(int id) {
+    @Step("Retorna uma reserva específica cadastrada usando Id")
+    public Response returnSpecificBookingWithId(int id) {
         return given()
                 .when()
                 .get("booking/" + id);
     }
 
-    @Step("Retorna lista Id' com filtros")
-    public Response bookingReturnIdsByFilter(String filter1, String value1,
-                                             String filter2, String value2,
-                                             String filter3, String value3) {
+    @Step("Retorna lista com Id's das Reservas cadastradas usando filtros")
+    public Response returnBookingIdsListWithFilters(String filter1, String value1,
+                                                    String filter2, String value2,
+                                                    String filter3, String value3) {
 
         return given()
-                .queryParams(filter1, value1,
-                            filter2, value2,
-                            filter3, value3)
+                .queryParams(filter1, value1, filter2, value2, filter3, value3)
                 .when()
                 .get("booking");
     }
