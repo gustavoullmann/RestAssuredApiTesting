0d63f714111f20b0d09b8178356a4ec76c794900
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-25 22:23:14 -0300

    update de nomes de métodos e steps

--- src/test/java/br/com/restassuredapitesting/tests/booking/requests/GetBookingRequest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/requests/GetBookingRequest.java
@@ -8,31 +8,31 @@ import static io.restassured.RestAssured.given;
 public class GetBookingRequest {
 
     public int returnBookingIdsListFirstId() {
-        return returnBookingIdsList()
+        return getBookingIdsList()
                 .then()
                 .statusCode(200)
                 .extract()
                 .path("[0].bookingid");
     }
 
-    @Step("Retorna lista com Id's das Reservas cadastradas")
-    public Response returnBookingIdsList() {
+    @Step("Retorna lista de Id's das Reservas cadastradas")
+    public Response getBookingIdsList() {
         return given()
                 .when()
                 .get("booking");
     }
 
     @Step("Retorna uma reserva específica cadastrada usando Id")
-    public Response returnSpecificBookingWithId(int id) {
+    public Response getSpecificBookingWithId(int id) {
         return given()
                 .when()
                 .get("booking/" + id);
     }
 
-    @Step("Retorna lista com Id's das Reservas cadastradas usando filtros")
-    public Response returnBookingIdsListWithFilters(String filter1, String value1,
-                                                    String filter2, String value2,
-                                                    String filter3, String value3) {
+    @Step("Retorna lista de Id's das Reservas cadastradas usando filtros")
+    public Response getBookingIdsListWithFilters(String filter1, String value1,
+                                                 String filter2, String value2,
+                                                 String filter3, String value3) {
 
         return given()
                 .queryParams(filter1, value1, filter2, value2, filter3, value3)
