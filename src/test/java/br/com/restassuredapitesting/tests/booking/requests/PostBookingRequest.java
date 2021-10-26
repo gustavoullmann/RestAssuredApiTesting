fad2d6135ace7772289d7bfbec81198eba3cf222
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-25 22:26:05 -0300

    exclusão de createInvalidBooking() e melhorias de nomes e descrições

--- src/test/java/br/com/restassuredapitesting/tests/booking/requests/PostBookingRequest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/requests/PostBookingRequest.java
@@ -1,6 +1,5 @@
 package br.com.restassuredapitesting.tests.booking.requests;
 
-import br.com.restassuredapitesting.tests.booking.payloads.BookingPayloads;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import org.json.JSONObject;
@@ -9,10 +8,8 @@ import static io.restassured.RestAssured.given;
 
 public class PostBookingRequest {
 
-    BookingPayloads bookingPayloads = new BookingPayloads();
-
-    @Step("Cria uma reserva")
-    public Response createBooking(JSONObject payload) {
+    @Step("Cria uma reserva com payload válido")
+    public Response postCreateBooking(JSONObject payload) {
         return given()
                 .header("Content-Type", "application/json")
                 .when()
@@ -20,17 +17,8 @@ public class PostBookingRequest {
                 .post("booking/");
     }
 
-    @Step("Cria uma reserva com payload incorreto")
-    public Response createInvalidBooking() {
-        return given()
-                .header("Content-Type", "application/json")
-                .when()
-                .body(bookingPayloads.invalidBookingPayload().toString())
-                .post("booking/");
-    }
-
     @Step("Cria uma reserva com header Accept inválido")
-    public Response createBookingInvalidAccept(JSONObject payload) {
+    public Response postCreateBookingWithWrongAcceptHeader(JSONObject payload) {
         return given()
                 .header("Accept", "-")
                 .header("Content-Type", "application/json")
