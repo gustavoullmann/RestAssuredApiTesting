b8da2393c031b860336b6105840ed09f876f74de
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-10-27 11:45:36 -0300

    criados métodos returnBookingIdsListFirstId() e createBookingIfIdListIsEmpty()

--- src/test/java/br/com/restassuredapitesting/utils/Utils.java
+++ src/test/java/br/com/restassuredapitesting/utils/Utils.java
@@ -1,6 +1,16 @@
 package br.com.restassuredapitesting.utils;
 
+import br.com.restassuredapitesting.tests.booking.payloads.BookingPayloads;
+import br.com.restassuredapitesting.tests.booking.requests.GetBookingRequest;
+import br.com.restassuredapitesting.tests.booking.requests.PostBookingRequest;
+import org.json.JSONObject;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+
 public class Utils {
+
+    GetBookingRequest getBookingRequest = new GetBookingRequest();
+    PostBookingRequest postBookingRequest = new PostBookingRequest();
 
     public static String getSchemaBasePath(String pack, String nameSchema) {
 
@@ -10,5 +20,31 @@ public class Utils {
                 + "/schema/"
                 + nameSchema
                 + ".json";
+    }
+
+    public int returnBookingIdsListFirstId() {
+
+        Integer firstId = getBookingRequest.getBookingIdsList()
+                .then()
+                .statusCode(200)
+                .extract()
+                .path("[0].bookingid");
+        if(firstId == null) {
+            firstId = createBookingIfIdListIsEmpty();
+        }
+        return firstId.intValue();
+    }
+
+    public Integer createBookingIfIdListIsEmpty() {
+
+        JSONObject booking1 = BookingPayloads.validBookingPayload1();
+        postBookingRequest.postCreateBooking(booking1)
+                .then()
+                .statusCode(200)
+                .body("bookingid", notNullValue());
+
+        Integer firstId = returnBookingIdsListFirstId();
+
+        return firstId;
     }
 }
